3c141f0dca1e7a8889e1bd980b33fff2ab079873
ShayZariyan <shayzariyan1@gmail.com>
2025-04-19 21:59:34 +0300

    TM

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 const dotenv = require('dotenv');
 dotenv.config();
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
